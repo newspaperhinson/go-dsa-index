0fd46c526727886d95404f549210c274e61302cb
newspaperhinson <newspaperhinson@gmail.com>
2025-05-07 20:57:34 +0800

    feat: implement generic insertion sort algorithm with custom comparator

--- pkg/algorithms/sorting/insertion_sort.go
+++ pkg/algorithms/sorting/insertion_sort.go
@@ -1,12 +1,12 @@
 package sorting
 
-func InsertionSort[T any](arr []T, compare func(a, b T) bool) {
+func InsertionSort[T any](arr []T, lessThan func(a, b T) bool) {
 	for i := 1; i < len(arr); i++ {
 		key := arr[i]
 
 		j := i - 1
 
-		for j >= 0 && compare(key, arr[j]) {
+		for j >= 0 && lessThan(key, arr[j]) {
 			arr[j+1] = arr[j]
 			j--
 		}
